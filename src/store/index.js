2d388234075cb58be7573ff938e280245e7fa5ae
ErickBraganca <e-braganca@hotmail.com>
2022-05-19 17:49:29 -0300

    feat: implemented state management

--- src/store/index.js
+++ src/store/index.js
@@ -2,27 +2,52 @@ import { createStore } from "vuex";
 
 const store = createStore({
   state: {
-    filesLoaded: [
-      { name: "Projeto de Estruturas", date: "18/05/2022" },
-      { name: "Projeto de Casas", date: "18/05/2022" },
-      { name: "Diagrama Elétrico", date: "18/05/2022" },
-    ],
+    loadedFiles: [],
     fileContent: [],
   },
   getters: {
-    getFilesLoaded: (state) => state.filesLoaded,
+    getLoadedFiles: (state) => state.loadedFiles,
+    getContentFile: (state) => state.fileContent,
   },
   mutations: {
-    setFilesLoaded(state, files) {
-      state.filesLoaded = files;
+    //Set state of historic of loaded files
+    setLoadedFiles(state, file) {
+      const files = state.loadedFiles;
+      files.unshift(file);
+    },
+    //Clean historic loaded files state
+    setEmptyFile(state) {
+      state.loadedFiles = [];
+    },
+    /*------------Content File-----------------*/
+    //Set file parsed content state
+    setContentFile(state, content) {
+      state.fileContent = content;
+    },
+    //Clean content loaded files state
+    setEmptyContent(state) {
+      state.fileContent = [];
     },
   },
   actions: {
-    async SetFilesLoaded({ commit }, files) {
-      commit("setFilesLoaded", files);
+    //Action to set historic loaded file mutation
+    SET_LOADED_FILES({ commit }, file) {
+      commit("setLoadedFiles", file);
+    },
+    //Action to set clean file historic mutation
+    SET_EMPTY_FILE({ commit }) {
+      commit("setEmptyFile");
+    },
+    /*------------Content File-----------------*/
+    //Action to set file parsed content mutation
+    SET_FILE_CONTENT({ commit }, content) {
+      commit("setContentFile", content);
+    },
+    //Action to set clean parsed content mutation
+    SET_EMPTY_CONTENT({ commit }) {
+      commit("setEmptyContent");
     },
   },
-  modules: {},
 });
 
 export default store;
